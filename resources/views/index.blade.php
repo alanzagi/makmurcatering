69cd0869adabac3833b38aacce61fc7832faad35
Alan <alanzagi2007@gmail.com>
2024-09-16 18:00:16 +0700

    pembaharuan halaman

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -60,6 +60,7 @@
             background-image: url('{{ asset('images/cateringbackground.jpg') }}');
             background-size: cover;
             background-position: center;
+            background-attachment: fixed;
         }
     </style>
 </head>
@@ -111,19 +112,46 @@
     <main class="h-screen bg-catering font-poppins flex items-center">
         <div class="absolute inset-0 bg-black opacity-40 z-0"></div>
         <div class="relative text-white z-10 flex flex-col items-start pl-4">
-            <h2 class="text-xl">Selamat Datang Di</h2>
-            <h1 class="text-3xl font-semibold text-yellow-400">Makmur Catering</h1>
-            <p class="text-md">Menyediakan kebutuhan catering sejak 2010</p>
+            <h2 class="text-xl md:text-3xl">Selamat Datang Di</h2>
+            <h1 class="text-3xl font-semibold text-yellow-400 md:text-5xl">Makmur Catering</h1>
+            <p class="text-md md:text-lg">Menyediakan kebutuhan catering sejak 2010</p>
         </div>
     </main>
 
     <section class="bg-white font-poppins">
-        <div class="px-10 py-8 text-center space-y-2">
-            <p class="text-xl font-semibold uppercase">"kualitas menu terbaik dan <br>dengan harga yang
+        <div class="px-10 py-8 text-center space-y-2 lg:pt-14">
+            <p class="text-xl font-semibold uppercase">"kualitas menu terbaik <br>dengan harga yang
                 terjangkau."</p>
             <h2 class="text-sm text-yellow-400 font-semibold uppercase">- Owner Kami Alan Zagi -</h2>
             <h1 class="text-4xl font-euphoria">Alan Zagi</h1>
         </div>
+
+        <div class="px-6 grid grid-cols-1 gap-y-6 pb-10 md:grid-cols-3 md:gap-y-0 md:gap-x-6 lg:px-40 lg:pb-20">
+            <div class="flex">
+                <img src="images/cateringbackground.jpg" class="rounded-md object-cover w-full h-full" alt="">
+            </div>
+
+            <div class="flex">
+                <img src="images/cateringbackground.jpg" class="rounded-md object-cover w-full h-full" alt="">
+            </div>
+
+            <div class="p-10 bg-white border border-gray-200 rounded-lg shadow flex flex-col justify-center">
+                <div class="text-center space-y-3 px-5">
+                    <h5 class="mb-2 text-2xl font-euphoria tracking-tight text-gray-900">
+                        Mengenal
+                    </h5>
+                    <h1 class="text-3xl mb-3 font-bold uppercase text-gray-700">kisah kami</h1>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="h-7 mx-auto">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z" />
+                    </svg>
+                    <p class="text-lg">Kami berkomitmen untuk menyajikan hidangan lezat, sehat, dan berkualitas tinggi
+                        dengan layanan profesional untuk setiap kebutuhan Anda.</p>
+                </div>
+            </div>
+        </div>
+
     </section>
 
     {{-- Flowbite Javascript --}}
